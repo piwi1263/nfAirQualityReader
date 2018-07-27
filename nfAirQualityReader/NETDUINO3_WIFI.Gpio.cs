cdd1755fe283e51278ede41a0917c8d805fabe78
piwi1263 <piwi1263@gmail.com>
2018-07-27 22:02:39 +0200

    Initial import

---
+++ nfAirQualityReader/NETDUINO3_WIFI.Gpio.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace nanoFramework.Targets.NETDUINO3_WIFI
+{
+	public static class Gpio
+	{
+        /// <summary>
+        /// 
+        /// </summary>
+        public const int UserButton = PB5;
+        public const int LinkLed = PC9;
+        public const int PowerLed = PC13;
+        public const int StateLed = PA8;
+        public const int UserLed = PA10;
+        public const int Goport1Led = PE9;
+        public const int Goport2Led = PE11;
+        public const int Goport3Led = PE14;
+        public const int D0 = PC7;
+        public const int D1 = PC6;
+        public const int D7 = PA1;
+        public const int D8 = PA0;
+        public const int D9 = PE5;
+        public const int D10 = PB10;
+        public const int D11 = PB15;
+        public const int D12 = PB14;
+        public const int D13 = PB13;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const int PA0 = 0;
+        public const int PA1 = 1;
+        public const int PA2 = 2;
+        public const int PA3 = 3;
+        public const int PA4 = 4;
+        public const int PA5 = 5;
+        public const int PA6 = 6;
+        public const int PA7 = 7;
+        public const int PA8 = 8;
+        public const int PA9 = 9;
+        public const int PA10 = 10;
+        public const int PA11 = 11;
+        public const int PA12 = 12;
+        public const int PA13 = 13;
+        public const int PA14 = 14;
+        public const int PA15 = 15;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const int PB0 = 16;
+        public const int PB1 = 17;
+        public const int PB2 = 18;
+        public const int PB3 = 19;
+        public const int PB4 = 20;
+        public const int PB5 = 21;
+        public const int PB6 = 22;
+        public const int PB7 = 23;
+        public const int PB8 = 24;
+        public const int PB9 = 25;
+        public const int PB10 = 26;
+        public const int PB11 = 27;
+        public const int PB12 = 28;
+        public const int PB13 = 29;
+        public const int PB14 = 30;
+        public const int PB15 = 31;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const int PC0 = 32;
+        public const int PC1 = 33;
+        public const int PC2 = 34;
+        public const int PC3 = 35;
+        public const int PC4 = 36;
+        public const int PC5 = 37;
+        public const int PC6 = 38;
+        public const int PC7 = 39;
+        public const int PC8 = 40;
+        public const int PC9 = 41;
+        public const int PC10 = 42;
+        public const int PC11 = 43;
+        public const int PC12 = 44;
+        public const int PC13 = 45;
+        public const int PC14 = 46;
+        public const int PC15 = 47;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const int PD0 = 48;
+        public const int PD1 = 49;
+        public const int PD2 = 50;
+        public const int PD3 = 51;
+        public const int PD4 = 52;
+        public const int PD5 = 53;
+        public const int PD6 = 54;
+        public const int PD7 = 55;
+        public const int PD8 = 56;
+        public const int PD9 = 57;
+        public const int PD10 = 58;
+        public const int PD11 = 59;
+        public const int PD12 = 60;
+        public const int PD13 = 61;
+        public const int PD14 = 62;
+        public const int PD15 = 63;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const int PE0 = 64;
+        public const int PE1 = 65;
+        public const int PE2 = 66;
+        public const int PE3 = 67;
+        public const int PE4 = 68;
+        public const int PE5 = 69;
+        public const int PE6 = 70;
+        public const int PE7 = 71;
+        public const int PE8 = 72;
+        public const int PE9 = 73;
+        public const int PE10 = 74;
+        public const int PE11 = 75;
+        public const int PE12 = 76;
+        public const int PE13 = 77;
+        public const int PE14 = 78;
+        public const int PE15 = 79;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const int PF0 = 80;
+        public const int PF1 = 81;
+        public const int PF2 = 82;
+        public const int PF3 = 83;
+        public const int PF4 = 84;
+        public const int PF5 = 85;
+        public const int PF6 = 86;
+        public const int PF7 = 87;
+        public const int PF8 = 88;
+        public const int PF9 = 89;
+        public const int PF10 = 90;
+        public const int PF11 = 91;
+        public const int PF12 = 92;
+        public const int PF13 = 93;
+        public const int PF14 = 94;
+        public const int PF15 = 95;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public const int PG0 = 96;
+        public const int PG1 = 97;
+        public const int PG2 = 98;
+        public const int PG3 = 99;
+        public const int PG4 = 100;
+        public const int PG5 = 101;
+        public const int PG6 = 102;
+        public const int PG7 = 103;
+        public const int PG8 = 104;
+        public const int PG9 = 105;
+        public const int PG10 = 106;
+        public const int PG11 = 107;
+        public const int PG12 = 108;
+        public const int PG13 = 109;
+        public const int PG14 = 110;
+        public const int PG15 = 111;
+    }
+}
---
+++ nfAirQualityReader/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Threading;
+
+using Windows.Devices.Gpio;
+using Windows.Devices.SerialCommunication;
+using Windows.Storage.Streams;
+
+using nanoFramework.Targets.NETDUINO3_WIFI;
+
+namespace nfAirQualityReader
+{
+    public class Program
+    {
+        #region The Locals
+
+        // Heartbeat LED
+        private static GpioPin _ledUser;
+
+        // The Particle Matter sensor requires 9600,8,N,1 on serial
+        private static SerialDevice _sds011;
+
+        #endregion
+
+        public static void Main()
+        {
+            try
+            {
+                Console.WriteLine("\rHello world of nanoFramework!\r");
+
+                // Setup the signal LED
+                InitGpio();
+
+                // Setup the serial communication with the sensor
+                InitSensor();
+
+                // And watch your console output
+                for (; ; )
+                {
+                    _ledUser.Write(_ledUser.Read() == GpioPinValue.Low ? GpioPinValue.High : GpioPinValue.Low);
+                    Thread.Sleep(500);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Don't leave thread 
+                Console.WriteLine(ex.ToString());
+
+                for (; ; )
+                {
+                    _ledUser.Write(_ledUser.Read() == GpioPinValue.Low ? GpioPinValue.High : GpioPinValue.Low);
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        private static void InitSensor()
+        {
+            // Since we only do a read the sensor on COM6 on pin D1 = PC06 = TX and D0 = PC07 = RX
+
+            //Open the port
+            _sds011 = SerialDevice.FromId("COM6");
+
+            // Set the usual stuff required for a serial connection
+            _sds011.BaudRate = 9600;
+            _sds011.DataBits = 8;
+            _sds011.Parity = SerialParity.None;
+            _sds011.StopBits = SerialStopBitCount.One;
+            _sds011.Handshake = SerialHandshake.None;
+
+            // Timeouts of 5 secs
+            _sds011.ReadTimeout = new TimeSpan(0, 0, 5);
+
+            // Reader for the output/input streams
+            DataReader reader = new DataReader(_sds011.InputStream);
+
+            // Set a watch for the return character
+            _sds011.WatchChar = (char)0xAB;
+
+            // Now register for the data received event
+            _sds011.DataReceived += _sds011_DataReceived;
+        }
+
+        private static void _sds011_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (e.EventType == SerialData.Chars)
+            {
+                // Ignore the in between noise
+            }
+            else if (e.EventType == SerialData.WatchChar)
+            {
+                SerialDevice serDev = (SerialDevice)sender;
+
+                using (DataReader dr = new DataReader(serDev.InputStream))
+                {
+                    dr.InputStreamOptions = InputStreamOptions.Partial;
+                    uint bytesRead = dr.Load(serDev.BytesToRead);
+
+                    if (bytesRead > 0)
+                    {
+                        byte[] rawData = new byte[bytesRead];
+                        dr.ReadBytes(rawData);
+
+                        // If rawData.Length == 10 and rawData[0] = 0xAA and rawData[9] = 0xAB and rawData[1] = 0xC0
+                        // this means we have a valid measure package from the sensor
+                        // and byte[2] = low byte, byte[3] = high byte of uint representing the AQI for PM 2.5
+                        if (rawData.Length >= 10)
+                        {
+                            if ((rawData[0] == 0xAA) && (rawData[1] == 0xC0) && (rawData[9] == 0xAB))
+                            {
+                                // Need to do checksum
+                                byte crc = 0;
+                                for (int i = 0; i < 6; i++)
+                                {
+                                    crc += rawData[i + 2];
+                                }
+                                if (crc == rawData[8])
+                                {
+                                    // All right, we have a go !!!!
+                                    float pm25 = 0, pm10 = 0;
+
+                                    pm25 = (float)((int)rawData[2] | (int)(rawData[3] << 8)) / 10;
+                                    pm10 = (float)((int)rawData[4] | (int)(rawData[5] << 8)) / 10;
+
+                                    Console.WriteLine(String.Format("Air quality index: {0}\tPM 10\t{1} µg / m3\tPM 2.5\t{2} µg / m3\tSensor: {3}",
+                                        DateTime.UtcNow.ToString("yyyy'-'MM'-'dd'\t'HH':'mm':'ss"),
+                                        pm10.ToString("N1"),
+                                        pm25.ToString("N1"),
+                                        "SDS011"));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void InitGpio()
+        {
+            _ledUser = GpioController.GetDefault().OpenPin(Gpio.UserLed);
+            _ledUser.SetDriveMode(GpioPinDriveMode.Output);
+        }
+    }
+}
